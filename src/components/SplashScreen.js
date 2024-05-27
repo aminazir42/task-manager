10a9e88b58cb530236470ee1967bdb1fbedf43ba
Amina Nazir <aminazir23@gmail.com>
2024-05-27 14:21:49 +0500

    jj

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { Box, Image } from "@chakra-ui/react";
@@ -6,7 +5,7 @@ import { Box, Image } from "@chakra-ui/react";
 function SplashScreen() {
   return (
     <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
-      <Image src="/public/logo.png" alt="Logo" />
+      <Image src="/path/to/logo.png" alt="Logo" />
     </Box>
   );
 }
